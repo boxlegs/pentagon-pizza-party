56a58379d1a207ce7f68821732c590cd4de1d3e9
boxlegs <tobyturns133@gmail.com>
2025-06-16 19:25:56 +1000

    Added populartimes calls

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pentagon-pizza-party
 impending geopolitical conflict can be predirect via the pentagon pizza index. this tool repeatedly polls the nearby dominos and notifies you if activity is higher than usual.
+
+This requires the installation of 
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 import requests
 import os
 from dotenv import load_dotenv
+import populartimes
 
 load_dotenv() 
 
 GCP_API_KEY = os.getenv('GCP_API_KEY')
 NTFY_URL = os.getenv('NTFY_URL')
 
-def get_dominos_data():
-    pass
+PLACE_ID = "ChIJI6ACK7q2t4kRFcPtFhUuYhU" # Domino's Pizza, 2602 Columbia Pike, Arlington, VA
 
+def get_dominos_data():
+    # Todo - add Freddie's Beach Bar
+    data = populartimes.get_id(GCP_API_KEY, PLACE_ID)
+    
+    # Make sure we don't try and get current popularity data if it's not available (i.e. if the place is closed)
+    print(data)
+    
+    
 def publish_notification(content, title, priority, tags):
     
     req = requests.post(NTFY_URL+"/dominos",
@@ -27,6 +35,7 @@ def notify():
      
 
 def main():
+    get_dominos_data()
     notify()
     if not GCP_API_KEY:
         print("GCP_API_KEY is not set in the .env file.")
