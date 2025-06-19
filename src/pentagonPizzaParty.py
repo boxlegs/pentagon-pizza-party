ad8b43b0d70ca20b6b653ac479d7402c56f70863
boxlegs <tobyturns133@gmail.com>
2025-06-19 11:44:46 +1000

    Changed name, cleaned up etc

--- src/pentagonPizzaParty.py
+++ src/pentagonPizzaParty.py
@@ -4,21 +4,17 @@ from dotenv import load_dotenv
 import populartimes
 from datetime import datetime
 import pytz
+import sys
 
 load_dotenv() 
 
 GCP_API_KEY = os.getenv('GCP_API_KEY')
-NTFY_URL = os.getenv('NTFY_URL')
+NTFY_URL = os.getenv('NTFY_URL') # Endpoint e.g. https://ntfy.sh/pentagon
 ANOMALY_THRESHOLD = float(os.getenv('THRESHOLD', 1.5)) # Default threshold is 1.5x
-TESTING_PLACE_ID = os.getenv('TESTING_PLACE_ID') # Default to Sydney Opera House
 PLACE_ID =  "ChIJI6ACK7q2t4kRFcPtFhUuYhU" # Domino's Pizza, 2602 Columbia Pike, Arlington, VA
 
 
 def get_dominos_data():
-    
-    # Todo - add Freddie's Beach Bar
-    # Todo - add Variable warning signoffs
-    # Todo - add tiers of warning notifications based on activity level
     
     # Get current time for comparison
     data = populartimes.get_id(GCP_API_KEY, PLACE_ID)
@@ -33,7 +29,7 @@ def get_dominos_data():
     
 def publish_notification(content, title, priority, tags):
     
-    req = requests.post(NTFY_URL+"/dominos",
+    req = requests.post(NTFY_URL,
     data=content,
     headers={
         "Title": title,
@@ -47,8 +43,15 @@ def publish_notification(content, title, priority, tags):
 
 def main():
     
+    if len(sys.argv) > 1:
+        print("Usage: python3 pentagonPizzaParty.py")
+        exit(1)
+        
     if not GCP_API_KEY:
-        print("GCP_API_KEY is not set in the .env file.")
+        print("GCP_API_KEY is not set - add it to your .env file or .[ba|z|k]shrc config")
+        exit(1)
+    if not NTFY_URL:
+        print("NTFY_URL is not set - add it to your .env file or .[ba|z|k]shrc config.")
         exit(1)
         
     # Get time in Virginia
@@ -70,7 +73,10 @@ def main():
         f"{current_hour % 12}{'PM' if current_hour >= 12 else 'AM'} by a factor of {current_popularity/regular_popularity:.1f}.\n" + \
         f"Current business is reading {current_popularity}, compared to average activity of {regular_popularity}.\n\n{'Shit is about to get variably real.' if current_popularity/regular_popularity > 2 else ''}"
         
-        publish_notification(content=content, title="MASSIVE ACTIVITY DETECTED at the Pentagon Domino's", priority="urgent", tags="biohazard,pizza")
+        publish_notification(content=content, # Feel free to edit yourself
+            title="MASSIVE ACTIVITY DETECTED at the Pentagon Domino's", 
+            priority="urgent", 
+            tags="biohazard,pizza")
 
     return
     
